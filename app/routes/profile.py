ef5094d06c001a55cd6ac8e09d7fe7a7172e4c10
lcnleunkeu <lcnleunkeu@etu.uqac.ca>
2025-05-28 14:42:21 +0000

    Refractoring du code et navigation dans les vues

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -11,7 +11,7 @@ def profile():
     """
     Afficher et Mettre à jour le profil d'un utilisateur
     """
-    logging.debug(f"Profile start: {session.get(user_id)}")
+    logging.debug(f"Profile start: {session}")
     if 'user_id' not in session:
         flash("Veuillez vous connecter avant d'accéder au profil.", "error")
         return redirect(url_for('auth.login'))
@@ -25,27 +25,8 @@ def profile():
 
     logging.debug(f"User data in profile : {user_data}")
 
-    user_data = get_user_details(session.get('user_id'))
-
-    # En cas d'erreur rediriger vers la page d'accueil
-    if 'error' in user_data:
-        flash(user_data['error'], "error")
-        return redirect(url_for('auth.login'))
-
-    logging.debug(f"User data in profile : {user_data}")
-
     if request.method == 'POST':
         # Mettre à jour les infos du profil
-        user_updated = updated_user(session.get('user_id'), request.form.get('display_name'))
-        logging.debug(f"User updated : {user_updated}")
-        
-        if user_updated :
-            flash("Profil mis à jour avec succès.", "success")
-            user_data["name"] = user_updated.display_name
-        else :
-            flash("Erreur lors de la mise à jour du profil.", "error")
-
-    return render_template('profile.html', user=user_data)
         user_updated = updated_user(session.get('user_id'), request.form.get('display_name'))
         logging.debug(f"User updated : {user_updated}")
         
